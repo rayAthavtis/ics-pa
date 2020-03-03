971d065706a6c8d8ad2fdf17faf6ce6d3c1445da
tracer-ics2017 <tracer@njuics.org>
2020-03-03 17:44:22 +0800

    >  compile 1711437 renlei Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  17:44:22 up  3:31,  1 user,  load average: 0.08, 0.03, 0.01 a30c4b69c3810353d267adcefa3bd2c02f883911

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,18 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args);
+
+static int cmd_info(char *args);
+
+static int cmd_x(char *args);
+
+static int cmd_p(char *args);
+
+static int cmd_w(char *args);
+
+static int cmd_d(char *args);
+
 static struct {
   char *name;
   char *description;
@@ -49,6 +61,12 @@ static struct {
 
   /* TODO: Add more commands */
 
+  { "si", "args:[N]; Execute [N] instructions step by step; N default 1", cmd_si },
+  { "info", "args:r/w; Print information about registers or watchpoint", cmd_info },
+  { "p", "Expr", cmd_p },
+  { "x", "x [N] [EXPR]; Scan the memory", cmd_x },
+  { "w", "Set the watchpoint", cmd_w },
+  { "d", "Delete the watchpoint", cmd_d },
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
@@ -73,6 +91,85 @@ static int cmd_help(char *args) {
     }
     printf("Unknown command '%s'\n", arg);
   }
+  return 0;
+}
+
+static int cmd_si(char *args) {
+  uint64_t N=0;
+  if (args == NULL) {
+    /* default */
+	N=1;
+  }
+  else {
+    int nRet=sscanf(args, "%llu", &N);
+	if (nRet<=0) {
+	  printf("args error in cmd_si\n");
+	  return 0;
+	}
+  }
+  cpu_exec(N);
+  return 0;
+}
+
+static int cmd_info(char *args) {
+  char s;
+  if (args == NULL) {
+    printf("args error in cmd_info\n");
+	return 0;
+  }
+  int nRet=sscanf(args, "%c", &s);
+  if (nRet<=0) {
+    printf("args error in cmd_info\n");
+	return 0;
+  }
+  if (s=='r') {  /* print regs */
+    int i;
+	for (i=0; i<8; i++)
+		printf("%s		0x%x\n", regsl[i], reg_l(i));
+	printf("eip		0x%x\n", cpu.eip);
+	for (i=0; i<8; i++)
+		printf("%s		0x%x\n", regsw[i], reg_w(i));
+	for (i=0; i<8; i++)
+		printf("%s		0x%x\n", regsb[i], reg_b(i));
+	return 0;
+  }
+  if (s=='w') {
+	printf("arg 'w' not finished yet\n");
+	return 0;
+  }
+  printf("args error in cmd_info\n");
+  return 0;
+}
+
+static int cmd_x(char *args) {
+  int nLen=0;
+  int i;
+  vaddr_t addr;
+  int nRet=sscanf(args, "%d		0x%x", &nLen, &addr);
+  if (nRet<=0) {
+    printf("args error in cmd_x\n");
+	return 0;
+  }
+  printf("Memory:\n");
+  for (i=0; i<nLen; i++) {
+    if (i%4==0)
+	  printf("\n0x%x:		0x%02x", addr+i, vaddr_read(addr+i, 1));
+	else
+	  printf("		0x%02x", vaddr_read(addr+i, 1));
+  }
+  printf("\n");
+  return 0;
+}
+
+static int cmd_p(char *args) {
+  return 0;
+}
+
+static int cmd_w(char *args) {
+  return 0;
+}
+
+static int cmd_d(char *args) {
   return 0;
 }
 
