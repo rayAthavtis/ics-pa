02d328b3fdfd2266b14bb34a1157fcfd554e6d01
tracer-ics2017 <tracer@njuics.org>
2020-03-07 19:20:06 +0800

    >  compile 1711437 renlei Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  19:20:06 up 16:56,  1 user,  load average: 0.00, 0.03, 0.01 6ae0a6c8353dd70be89ba0bfd8027c8ee97ed06c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -149,12 +149,12 @@ static bool ck_prt(int lp, int rp) {
   int ct=0;
   if (tokens[lp].type != TK_LP || tokens[rp].type != TK_RP)
 	return false;
-  for (i=lp; i<rp; i++) {
+  for (i=lp-1; i<rp; i++) {
     if (tokens[i].type == TK_LP)
 	  ct++;
 	else if(tokens[i].type == TK_RP)
 	  ct--;
-	if (ct<1)
+	if (ct<0)
 	  return false;
   }
   if (ct>0) {
