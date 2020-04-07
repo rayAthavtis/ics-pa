100a955af02f7f25ae3f0b725f396b6156f0a647
tracer-ics2017 <tracer@njuics.org>
2020-04-07 23:10:31 +0800

    >  compile 1711437 renlei Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  23:10:31 up 2 days, 12:21,  1 user,  load average: 0.17, 0.15, 0.17 c461a5b138bda3c922cd8d3fd87848b6b0b3ae

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -46,8 +46,8 @@ void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
   // TODO();
-  t2 = pio_read(id_src->val, id_src->width);
-  operand_write(id_dest, &t2);
+  /* t2 = pio_read(id_src->val, id_src->width);
+  operand_write(id_dest, &t2); */
 
   print_asm_template2(in);
 
@@ -58,7 +58,7 @@ make_EHelper(in) {
 
 make_EHelper(out) {
   // TODO();
-  pio_write(id_dest->val, id_src->width, id_src->val);
+  // pio_write(id_dest->val, id_src->width, id_src->val);
 
   print_asm_template2(out);
 
