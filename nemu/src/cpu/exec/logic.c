77e64554009637d088d21f22edaeaff7067ab89f
tracer-ics2017 <tracer@njuics.org>
2020-04-08 17:17:55 +0800

    >  compile 1711437 renlei Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  17:17:55 up 2 days, 14:21,  1 user,  load average: 0.01, 0.19, 0.23 a9bfea77ae449cb5fdb057f7dcd475c1fa6e65bc

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -110,7 +110,20 @@ make_EHelper(not) {
 }
 
 make_EHelper(rol) {
-  TODO();
+  // TODO();
+  rtl_shl(&t0, &id_dest->val, &id_src->val);
+  t3 = 0;
+  if (decoding.is_operand_size_16)
+  	rtl_addi(&t1, &t3, 16);
+  else
+	rtl_addi(&t1, &t3, 32);
+  rtl_sub(&t1, &t1, &id_src->val);
+  rtl_shr(&t2, &id_dest->val, &t1);
+
+  rtl_or(&t0, &t0, &t2);
+  operand_write(id_dest, &t0);
+
+  rtl_update_ZFSF(&t0, id_dest->width);
 
   print_asm_template2(rol);
 }
