834712fb42682d9bd5e83a63ac80b3dff412f57a
tracer-ics2017 <tracer@njuics.org>
2020-04-25 18:47:42 +0800

    >  compile 1711437 renlei Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  18:47:42 up 3 days, 12:05,  1 user,  load average: 0.30, 0.10, 0.03 ecc0c7d4d1ec18c64874ba95564f76b278c7c80

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -47,7 +47,7 @@ static inline void rtl_mul(rtlreg_t* dest_hi, rtlreg_t* dest_lo, const rtlreg_t*
   asm volatile("mul %3" : "=d"(*dest_hi), "=a"(*dest_lo) : "a"(*src1), "r"(*src2));
 }
 
-static inline void rtl_imul(rtlreg_t* dest_hi, rtlreg_t* dest_lo, const rtlreg_t* src1, const rtlreg_t* src2) {
+inline void rtl_imul(rtlreg_t* dest_hi, rtlreg_t* dest_lo, const rtlreg_t* src1, const rtlreg_t* src2) {
   asm volatile("imul %3" : "=d"(*dest_hi), "=a"(*dest_lo) : "a"(*src1), "r"(*src2));
 }
 
