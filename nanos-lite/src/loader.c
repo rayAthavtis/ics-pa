c9c5c2c757c960979421b201cfcb6aabedb87547
tracer-ics2017 <tracer@njuics.org>
2020-05-09 23:14:06 +0800

    >  run 1711437 renlei Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  23:14:06 up 3 days, 16:19,  1 user,  load average: 0.14, 0.12, 0.09 bdf207dfd867490cbb59e3c0e6d0bb5243d645

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -7,6 +7,6 @@ extern void ramdisk_read(void *buf, off_t offset, size_t len);
 
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
-  ramdisk_read(DEFAULT_ENTRY, 0, ramdisk_start-ramdisk_end);
+  ramdisk_read(DEFAULT_ENTRY, 0, (ramdisk_end-ramdisk_start));
   return (uintptr_t)DEFAULT_ENTRY;
 }
