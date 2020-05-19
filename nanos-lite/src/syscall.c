ae3cf2c3f5d55c5315cdd2a7699b0aa1cd9d0ee7
tracer-ics2017 <tracer@njuics.org>
2020-05-19 17:21:10 +0800

    >  run 1711437 renlei Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  17:21:10 up 4 days, 40 min,  1 user,  load average: 0.51, 0.35, 0.24 c9830fc4931301f56ddbc99cd3fdbac7a5ae8b96

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,8 +1,6 @@
 #include "common.h"
 #include "syscall.h"
 
-extern char _end;
-
 uintptr_t sys_none() {
   return 1;
 }
@@ -38,7 +36,7 @@ _RegSet* do_syscall(_RegSet *r) {
 
   switch (a[0]) {
 	case SYS_none: SYSCALL_ARG1(r) = sys_none(); break;
-	case SYS_exit: sys_exit(0); break;
+	case SYS_exit: sys_exit(a[1]); break;
 	case SYS_write: SYSCALL_ARG1(r) = sys_write(a[1], a[2], a[3]); break;
 	case SYS_brk: SYSCALL_ARG1(r) = sys_brk(a[1]); break;
 	
