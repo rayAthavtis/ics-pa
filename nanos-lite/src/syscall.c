a464b374f9daa1982ab69e6cbb134edd48a1b81e
tracer-ics2017 <tracer@njuics.org>
2020-05-19 16:17:14 +0800

    >  run 1711437 renlei Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  16:17:14 up 3 days, 23:36,  1 user,  load average: 0.00, 0.01, 0.00 7e99915ef876efd242f05798ff804de5eff85c1d

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,16 +9,29 @@ void sys_exit(int code) {
   _halt(code);
 }
 
+ssize_t sys_write(int fd, uintptr_t buf, size_t len) {
+  if (fd==1 || fd==2) {
+    char ch;
+	for (int i=0; i<len; i++) {
+	  memcpy(&ch, (void *)buf+i, 1);
+	  _putc(ch);
+	}
+	return len;
+  }
+  return -1;
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
-  // a[1] = SYSCALL_ARG2(r);
-  // a[2] = SYSCALL_ARG3(r);
-  // a[3] = SYSCALL_ARG4(r);
+  a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
 	case SYS_none: SYSCALL_ARG1(r) = sys_none(); break;
 	case SYS_exit: sys_exit(0); break;
+	case SYS_write: SYSCALL_ARG1(r) = sys_write(a[1], a[2], a[3]); break;
 	
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
