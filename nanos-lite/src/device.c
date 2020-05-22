11c3d91c04da1aaee6d4334faf58b9f25d16a1eb
tracer-ics2017 <tracer@njuics.org>
2020-05-22 11:25:59 +0800

    >  run 1711437 renlei Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  11:25:59 up 4 days,  7:18,  1 user,  load average: 0.77, 0.56, 0.33 20f552e494f50b27207cd7222b600cb6c45bb6bc

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -13,8 +13,8 @@ size_t events_read(void *buf, size_t len) {
   char key_state = (key & 0x8000) ? 'd' : 'u';
   if (key_state=='d') { key ^= 0x8000; }
   if (key!=_KEY_NONE)
-  { return snprintf(buf, len, "k%c %s", key_state, keyname[key]) - 1; }
-  else { return snprintf(buf, len, "t %d", _uptime()) - 1; }
+  { return snprintf(buf, len, "k%c %s\n", key_state, keyname[key]) - 1; }
+  else { return snprintf(buf, len, "t %d\n", _uptime()) - 1; }
 
   return 0;
 }
