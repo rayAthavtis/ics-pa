65fb3b54c341e53cbff0832e6a594c89cf42e582
tracer-ics2017 <tracer@njuics.org>
2020-05-22 14:07:08 +0800

    >  run 1711437 renlei Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  14:07:08 up 4 days,  8:54,  1 user,  load average: 3.46, 3.17, 2.76 ba5892f56f8371a2b62e886c7c27ae614b3832d6

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -33,7 +33,7 @@ int main() {
   // uint32_t entry = loader(NULL, NULL);
   // uint32_t entry = loader(NULL, "/bin/text");
   // uint32_t entry = loader(NULL, "/bin/events");
-  uint32_t entry = loader(NULL, "/bin/videotest");
+  uint32_t entry = loader(NULL, "/bin/pal");
   ((void (*)(void))entry)();
 
   panic("Should not reach here");
