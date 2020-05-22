131854ffbae099a6218e36b26da10f7b7507bcf2
tracer-ics2017 <tracer@njuics.org>
2020-05-22 16:30:29 +0800

    >  compile 1711437 renlei Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  16:30:29 up 4 days, 11:17,  1 user,  load average: 3.44, 3.17, 3.29 8e1ff741c673ad375b939a706072e357158b5443

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -23,23 +23,24 @@ size_t events_read(void *buf, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
-  memcpy(buf, dispinfo + offset, len);
+  strncpy(buf, dispinfo + offset, len);
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
-  int ly = (offset + len) / sizeof(uint32_t) / _screen.width;
+  // int ly = (offset + len) / sizeof(uint32_t) / _screen.width;
   offset /= sizeof(uint32_t);
   int wx = offset % _screen.width;
   int wy = offset / _screen.width;
 
-  // _draw_rect(buf, wx, wy, len/sizeof(uint32_t), 1);
+  _draw_rect(buf, wx, wy, len/sizeof(uint32_t), 1);
+  /*
   if (ly==wy)
   { _draw_rect(buf, wx, wy, len / sizeof(uint32_t), 1); }
   else {
     int tmp = _screen.width - wx;
 	if (ly-wy==1) {
 	  _draw_rect(buf, wx, wy, tmp, 1);
-	  _draw_rect(buf+tmp*sizeof(uint32_t), 0, ly, len/sizeof(uint32_t), 1);
+	  _draw_rect(buf+tmp*sizeof(uint32_t), 0, ly, len/sizeof(uint32_t)-tmp, 1);
 	}
 	else {
 	  _draw_rect(buf, wx, wy, tmp, 1);
@@ -48,6 +49,7 @@ void fb_write(const void *buf, off_t offset, size_t len) {
 	  _draw_rect(buf+tmp*sizeof(uint32_t)+ny*_screen.width*sizeof(uint32_t), 0, ly, len/sizeof(uint32_t)-tmp-ny*_screen.width, 1);
 	}
   }
+  */
 }
 
 void init_device() {
@@ -56,4 +58,5 @@ void init_device() {
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
   sprintf(dispinfo, "WIDTH: %d\nHEIGHT: %d\n", _screen.width, _screen.height);
+  Log("dispinfo: %s", dispinfo);
 }
