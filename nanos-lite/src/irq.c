a7e8d9e736d0b016803b1a65d1e90d14d4f73bfc
tracer-ics2017 <tracer@njuics.org>
2020-06-09 12:23:26 +0800

    >  compile 1711437 renlei Linux ubuntu 4.15.0-88-generic #88~16.04.1-Ubuntu SMP Wed Feb 12 04:18:19 UTC 2020 i686 i686 i686 GNU/Linux  12:23:26 up  6:19,  1 user,  load average: 0.32, 0.20, 0.26 3a98f3978cbbcf8b42cb6723f483accf2976093

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,11 +6,14 @@ extern _RegSet* schedule(_RegSet *prev);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL:
-      // return do_syscall(r);
-      do_syscall(r);
-      return schedule(r);
+      return do_syscall(r);
+      // do_syscall(r);
+      // return schedule(r);
     case _EVENT_TRAP: 
       // printf("event: EVENT_TRAP !\n");
+      return schedule(r);
+    case _EVENT_IRQ_TIME:
+      Log("event: EVENT_IRQ_TIME works!");
       return schedule(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
